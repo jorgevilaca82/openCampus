536db528b4c2576f907db608e25c06082dfa8921
Jorge Vilaça <jorge.vilaca@gmail.com>
2019-09-10 13:56:26 -0400

    projeto base criado

---
+++ aqueduct/open_campus_api/lib/open_campus_api.dart
@@ -0,0 +1,12 @@
+/// open_campus_api
+///
+/// A Aqueduct web server.
+library open_campus_api;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+export 'package:aqueduct/managed_auth.dart';
+
+export 'channel.dart';
